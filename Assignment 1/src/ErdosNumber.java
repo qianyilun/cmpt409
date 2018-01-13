091defef48031962e70f34f23ef637db45e9bcae
qianyilun <qianyilun@gmail.com>
2018-01-13 13:49:38 -0800

    worked with inputted name list

--- Assignment 1/src/ErdosNumber.java
+++ Assignment 1/src/ErdosNumber.java
@@ -1,48 +1,66 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 /**
  * Created by yilunq on 12/01/18.
  */
 public class ErdosNumber {
-    public static void main(String[] args) {
-        List<String> input = new ArrayList<>();
-        String v1 = "A,B,Erdos";
-        String v2 = "Erdos,C";
-        String v3 = "A,D";
-        String v4 = "E, F";
-        input.add(v1);
-        input.add(v2);
-        input.add(v3);
-        input.add(v4);
+    public static void main(String[] args) throws IOException {
+        int nameSize = 2;
+        String[] names = new String[nameSize];
 
-        System.out.println(new ErdosNumber().findErdosNumber());
+        names[0] = "Smith, M.N.";
+        names[1] = "Hsueh, Z";
+//        names[1] = "Chen, X.";
+
+        Map<String, Set<String>> graph = new HashMap<>();
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line = br.readLine().trim();
+        int scenarios = Integer.parseInt(line);
+        for(int scenario = 0; scenario < scenarios; ++scenario) {
+            System.out.println("Scenario " + (scenario + 1));
+            String[] parts = br.readLine().trim().split("\\s+");
+            int P = Integer.parseInt(parts[0].trim());
+            int N = Integer.parseInt(parts[1].trim());
+
+            for(int p = 0; p < P; ++p) {
+                // 解析paper的构成
+                parts = br.readLine().trim().split("\\.[,\\:]");
+                for (int i = 0; i < parts.length; i++) {
+                    parts[i] = parts[i].trim() + ".";
+                }
+
+                // 构建图
+                initialGraph(graph, parts);
+            }
+
+            // 确认一下图的结构
+            System.out.println(graph);
+
+            findErdosNumber(graph, names);
+        }
     }
 
 
-    private int findErdosNumber() {
-        List<String> nameList = new ArrayList<String>();
-        nameList.add("D");
-
-        List<String> paperDatabase = new ArrayList<>();
-        paperDatabase.add("A");
-        paperDatabase.add("B");
-        paperDatabase.add("Erodos");
-
-        String[] papers = new String[]{"A, B, Erodos", "Erodos, C", "A, D", "E, F"};
-
-        // 1. initial graph
-        Map<String, Set<String>> graph = new HashMap<>();
-        for (int i = 0; i < papers.length; i++) {
-            initialGraph(graph, papers[i]);
+    private static int findErdosNumber(Map<String, Set<String>> graph, String[] names) {
+        for (int i = 0; i < names.length; i++) {
+            int result = bfs(names[i], graph);
+            if (result == -1) {
+                System.out.println(names[i] + " infinity");
+            } else {
+                System.out.println(names[i] + " "+ result);
+            }
         }
 
-        System.out.println(graph);
+        return -1;
+    }
 
-
-        // 2. bfs
+    private static int bfs(String name, Map<String, Set<String>> graph) {
         Queue<String> queue = new LinkedList<>();
-        queue.offer("D");
-
+        queue.offer(name);
         int edrosNumber = 0;
 
         while (!queue.isEmpty()) {
@@ -51,9 +69,12 @@ public class ErdosNumber {
             int size = queue.size();
             for (int i = 0; i < size; i++) {
                 String head = queue.poll();
-
+                // 若不存在图中
+                if (graph.get(head) == null) {
+                    break;
+                }
                 for (String neighbor : graph.get(head)) {
-                    if (neighbor.equals("Erodos")) {
+                    if (neighbor.equals("Erdos, P.")) {
                         return edrosNumber;
                     }
                     queue.offer(neighbor);
@@ -63,38 +84,21 @@ public class ErdosNumber {
         return -1;
     }
 
-    private void initialGraph(Map<String, Set<String>> graph,
-                              String paper) {
-        // parse paper database
-        String[] authors = paper.split(", ");
-//        System.out.println(Arrays.toString(authors));
-
+    private static void initialGraph(Map<String, Set<String>> graph,
+                              String[] papers) {
         // add other's names to its Set
-        for (String author : authors) {
+        for (String author : papers) {
             if (!graph.containsKey(author)) {
                 graph.put(author, new HashSet<String>());
             }
         }
 
-
-        for (int i = 0; i < authors.length; i++) {
-            for (int j = 0; j < authors.length; j++) {
+        for (int i = 0; i < papers.length; i++) {
+            for (int j = 0; j < papers.length; j++) {
                 if (i != j) {
-                    graph.get(authors[i]).add(authors[j]);
+                    graph.get(papers[i]).add(papers[j]);
                 }
             }
         }
-
-
-//        for (String name : nameList) {
-//            if (!graph.containsKey(name)) {
-//                graph.put(name, new HashSet<String>());
-//            }
-//            for (String author : authors) {
-//                if (!author.equals(name)) {
-//                    graph.get(name).add(author);
-//                }
-//            }
-//        }
     }
 }
