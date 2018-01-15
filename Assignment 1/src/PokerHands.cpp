89f7ba1ba6628a7d2f1d919d0bf06be827dfb4a3
Peter Tan <junyuant@sfu.ca>
2018-01-14 17:07:21 -0800

    Add logic for constrcting bit representation
    There are several calculations added:
    1. max_same_suit: represents the maximum cards in the same suit in a given poker hand
    2. pair_count: represents how many pairs exist for a given poker hand. A pair should contain at least 2 cards with the same value
    3. max_same_value: represents the maximum cards with the same value in a given poker hand
    4. max_consecutive: represents the max # of cards with a consecutive value
    5. highest_card: represents the highest value in a given poker hand

--- Assignment 1/src/PokerHands.cpp
+++ Assignment 1/src/PokerHands.cpp
@@ -42,7 +42,98 @@ tuple<vector<string>, vector<string>> split(const string& input, char delimit) {
 
 
 uint64_t construct_bit_representation(const vector<string>& pokerHands) {
+	map<char, bitset<13>> suit_map {{'C', 0}, {'D', 0}, {'H', 0}, {'S', 0}};
+	
+	// Used to store the highest value of a card in the poker hand
+	int highest_card = 0;
 
+	for (const auto& i : pokerHands) {
+		// cout << i[0] << endl;
+		int card_value = i[0] - '0'; // Converting from char to int
+		// cout << card_value << endl;
+		
+		// If the card value is not 2~9, map T, J, Q, K, A to 10~14
+		if ( !(card_value >= 2 && card_value <= 9) ) {
+			switch(i[0]) {
+				case 'T': card_value = 10; break;
+				case 'J': card_value = 11; break;
+				case 'Q': card_value = 12; break;
+				case 'K': card_value = 13; break;
+				case 'A': card_value = 14; break;
+			}
+		}
+
+		// Store the card in bit representation
+		int ind = 0 - (card_value - 2 - 12);
+		suit_map[ i[1] ][ind] = 1;
+
+
+		highest_card = (card_value > highest_card) ? card_value : highest_card;
+
+
+		// switch (i[1]) {
+		// 	case 'C': suit_map['C'] |= 1 << ( 0 - (card_value - 14) ); break;
+		// 	case 'D': suit_map['D'] |= 1 << ( 0 - (card_value - 14) ); break;
+		// 	case 'H': suit_map['H'] |= 1 << ( 0 - (card_value - 14) ); break;
+		// 	case 'S': suit_map['S'] |= 1 << ( 0 - (card_value - 14) ); break;
+		// 	default: break;
+		// }
+
+	}
+
+	// === Print bit representation of each suit ===
+
+	for (const auto& kv : suit_map) {
+		cout << kv.second << " ";
+	}
+	cout << endl;
+
+
+	// === Count max same value and # of pairs ===
+	int max_same_value = 0;
+	int pair_count = 0;
+
+	for (int i = 0; i < 13; i++) {
+		int sum = suit_map['C'][i] + suit_map['D'][i] + suit_map['H'][i] + suit_map['S'][i];
+		
+		max_same_value = (sum > max_same_value) ? sum : max_same_value;
+		if (sum > 1) {
+			++pair_count;
+		}
+	}
+	cout << "Max same value: " << max_same_value << endl;
+	cout << "Pair count: " << pair_count << endl;
+
+
+	// === Count max same suit ===
+	int max_same_suit = 0;
+	for (const auto& kv : suit_map) {
+		int num_bits_set = (int) kv.second.count();
+		max_same_suit = (num_bits_set > max_same_suit) ? num_bits_set : max_same_suit;
+	}
+	// cout << "Max same suit: " << max_same_suit << endl;
+
+	cout << "Highest card: " << highest_card << endl;
+
+
+	// === Count max consecutive cards ===
+	int max_consecutive = 0;
+
+	bitset<13> or_of_all_suits = 0;
+	for (const auto& kv : suit_map) {
+		or_of_all_suits |= kv.second;
+	}
+	// cout << or_of_all_suits << endl;
+	for (unsigned int i = 1; i < or_of_all_suits.size(); ++i) {
+		if (max_consecutive == 0 && or_of_all_suits[i] != or_of_all_suits[i - 1]) {
+			++max_consecutive;
+		} else if (or_of_all_suits[i] != 0 && or_of_all_suits[i] == or_of_all_suits[i - 1]) {
+			++max_consecutive;
+		}
+	}
+	// cout << "Max consecutive: " << max_consecutive << endl;
+
+	return -1;
 }
 
 
