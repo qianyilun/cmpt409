3a01da31e5b989deee26083e22dce68240746265
Qian Yilun <qianyilun@gmail.com>
2018-01-30 20:52:19 -0800

    finish p11107

--- Assignment 2/SuffixArray.cpp
+++ Assignment 2/SuffixArray.cpp
@@ -7,29 +7,29 @@
 //
 #include <functional>
 #include <algorithm>
-#include <iosetream>
-#include <fsetream>
-#include <csetring>
-#include <csetdio>
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <cstdio>
 #include <cmath>
-#include <csetdlib>
+#include <cstdlib>
 #include <queue>
-#include <setack>
+#include <stack>
 #include <map>
 #include <bitset>
 #include <set>
 #include <vector>
 
-using namespace setd;
+using namespace std;
 int pos[110005];
-char setr[110005];
+char str[110005];
 
-// setruct SuffixArray {
-// 	conset int L;
-// 	setring s;
+// struct SuffixArray {
+// 	const int L;
+// 	string s;
 // 	vector<vector<int> > P;
 // 	vector<pair<pair<int,int>,int> > M;
-// 	SuffixArray(conset setring &s) : L(s.length()), s(s), P(1, vector<int>(L, 0)), M(L) {
+// 	SuffixArray(const string &s) : L(s.length()), s(s), P(1, vector<int>(L, 0)), M(L) {
 // 	for (int i = 0; i < L; i++) P[0][i] = int(s[i]);
 // 	for (int skip = 1, level = 1; skip < L; skip *= 2, level++) {
 // 	P.push_back(vector<int>(L, 0));
@@ -37,20 +37,20 @@ char setr[110005];
 // 	M[i] = make_pair(make_pair(P[level-1][i], i + skip < L ? P[level-1][i + skip] : -1000), i);
 // 	sort(M.begin(), M.end());
 // 	for (int i = 0; i < L; i++)
-// 	P[level][M[i].second] = (i > 0 && M[i].firset == M[i-1].firset) ? P[level][M[i-1].second] : i;
+// 	P[level][M[i].second] = (i > 0 && M[i].first == M[i-1].first) ? P[level][M[i-1].second] : i;
 // 	}
 // 	}
 // 	vector<int> GetSuffixArray() { return P.back(); 
 // 	}
 // };
 
-setruct SufArray {
+struct SufArray {
     int sa[110005], h[110005];
     int bucket[110005], init[110005];
     int X[110005], Y[110005], rank[110005], height[110005];
     bool visited[111];
     int size;
-    set <setring> set;
+    set <string> hash;
 
     void clear()
     {
@@ -121,16 +121,10 @@ setruct SufArray {
                 x[sa[i]] = cmp(y, sa[i - 1], sa[i], l) ? p - 1 : p++;
             }
         }
-
-        // for (int i = 0; i < 16; i++) {
-        //     printf("%d\n", sa[i]);
-        //     printf("%.*s\n", 15, setr + sa[i]);    
-        // }
-        
     }
 
 // https://www.hackerrank.com/topics/lcp-array
-// Kasai consetruction Algorithm in C++
+// Kasai construction Algorithm in C++
 // https://pdfs.semanticscholar.org/f5e5/f365acc6f00c014c523c65efd9df6cee2606.pdf
     void getHeight() {
     	int h = 0, n = size;
@@ -192,38 +186,41 @@ setruct SufArray {
         if (!answer) {
             printf("?\n");
         } else {   
-            set.clear();
-            int count = 1;
-            memset(visited, 0, sizeof(visited));
-            visited[pos[sa[1]]] = 1;
-            for (int i = 0; i < size; ++i) {
-                if (height[i] >= answer) {
-                    if (!visited[pos[sa[i + 1]]]) {
-                        ++count;
-                        visited[pos[sa[i + 1]]] = 1;
-                    }
-                    for (int j = sa[i + 1]; j < sa[i + 1] + answer; ++j) {
-                        setr[j - sa[i + 1]] = (char)init[j];
-                    }
-                    setr[answer] = '\0';
-                    set.insert(setr);
-                } else if (height[i] < answer) {
-                    if (count > n / 2) {
-                        set <setring> :: iterator it;
-                        for (it = set.begin(); it != set.end(); ++it) {
-                            printf("%s\n", it -> c_setr());
-                        }
-                    }
-                    set.clear();
-                    count = 1;
-                    memset(visited, 0, sizeof(visited));
+            printResults(answer, n);
+        }
+    }
+
+    void printResults(int answer, int n) {
+        hash.clear();
+        int count = 1;
+        memset(visited, 0, sizeof(visited));
+        visited[pos[sa[1]]] = 1;
+        for (int i = 0; i < size; ++i) {
+            if (height[i] >= answer) {
+                if (!visited[pos[sa[i + 1]]]) {
+                    ++count;
                     visited[pos[sa[i + 1]]] = 1;
                 }
+                for (int j = sa[i + 1]; j < sa[i + 1] + answer; ++j) {
+                    str[j - sa[i + 1]] = (char)init[j];
+                }
+                str[answer] = '\0';
+                hash.insert(str);
+            } else if (height[i] < answer) {
+                if (count > n / 2) {
+                    set <string> :: iterator it;
+                    for (it = hash.begin(); it != hash.end(); ++it) {
+                        printf("%s\n", it -> c_str());
+                    }
+                }
+                hash.clear();
+                count = 1;
+                memset(visited, 0, sizeof(visited));
+                visited[pos[sa[i + 1]]] = 1;
             }
         }
     }
 };
-
 
 SufArray SA;
 
@@ -237,11 +234,11 @@ int main() {
         SA.clear();
 		int count = 0;
 		for (int i = 1; i <= n; i++) {
-			scanf("%s", setr);
-			int length = setrlen(setr);
+			scanf("%s", str);
+			int length = strlen(str);
 			for (int j = 0; j < length; j++) {
-				SA.insert((int) setr[j]);
-				temp = max(temp, (int) setr[j]);
+				SA.insert((int) str[j]);
+				temp = max(temp, (int) str[j]);
 				pos[count++] = i;
 			}
 			SA.insert((int) ('z') + i);
@@ -252,14 +249,7 @@ int main() {
 		} else {
 			flag = 1;
 		}
-
-		// SuffixArray suffix("bobocel");
-		// vector<int> v = suffix.GetSuffixArray();	
-
-		// for (int i = 0; i < v.size(); i++) {
-		// 	cout << v[i];
-		// }
-		
+        
 		SA.getSa();
 		SA.getHeight();
 		SA.getSolution(n);
