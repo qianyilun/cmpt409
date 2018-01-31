83dc442d838a43226ddb5c95d222d5c9405b8be7
Qian Yilun <qianyilun@gmail.com>
2018-01-30 20:52:46 -0800

    update SuffixArray.cpp

--- Assignment 2/SuffixArray.cpp
+++ Assignment 2/SuffixArray.cpp
@@ -24,26 +24,6 @@ using namespace std;
 int pos[110005];
 char str[110005];
 
-// struct SuffixArray {
-// 	const int L;
-// 	string s;
-// 	vector<vector<int> > P;
-// 	vector<pair<pair<int,int>,int> > M;
-// 	SuffixArray(const string &s) : L(s.length()), s(s), P(1, vector<int>(L, 0)), M(L) {
-// 	for (int i = 0; i < L; i++) P[0][i] = int(s[i]);
-// 	for (int skip = 1, level = 1; skip < L; skip *= 2, level++) {
-// 	P.push_back(vector<int>(L, 0));
-// 	for (int i = 0; i < L; i++)
-// 	M[i] = make_pair(make_pair(P[level-1][i], i + skip < L ? P[level-1][i + skip] : -1000), i);
-// 	sort(M.begin(), M.end());
-// 	for (int i = 0; i < L; i++)
-// 	P[level][M[i].second] = (i > 0 && M[i].first == M[i-1].first) ? P[level][M[i-1].second] : i;
-// 	}
-// 	}
-// 	vector<int> GetSuffixArray() { return P.back(); 
-// 	}
-// };
-
 struct SufArray {
     int sa[110005], h[110005];
     int bucket[110005], init[110005];
@@ -249,7 +229,7 @@ int main() {
 		} else {
 			flag = 1;
 		}
-        
+
 		SA.getSa();
 		SA.getHeight();
 		SA.getSolution(n);
