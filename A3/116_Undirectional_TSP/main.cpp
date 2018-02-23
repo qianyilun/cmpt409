581e250fd689d9228917a5ee670600f8f980b26c
Peter Tan <peter.tan01@sap.com>
2018-02-23 01:30:50 -0800

    Finished question #116

--- A3/116_Undirectional_TSP/main.cpp
+++ A3/116_Undirectional_TSP/main.cpp
@@ -3,11 +3,16 @@
 #include <cstdio>
 #include <cstring>
 #include <iostream>
+#include <map>
 #include <sstream>
 #include <string>
 #include <vector>
 
 using namespace std;
+
+pair<int, vector<int>> handle_memoization(int new_row, int new_col);
+
+
 
 template<class T>
 ostream& operator <<(ostream& os, vector<T> v) {
@@ -25,21 +30,33 @@ ostream& operator <<(ostream& os, vector<T> v) {
 static int row, col;
 static vector<vector<int>> data;
 
-int get_min(int r, int c) {
-    if (c == col - 1) {
-        return data[r][c];
-    } else if (c > col - 1) {
-        return 0;
+// Key: the coordinate (x, y) of grid data
+// Value: stores the total weight starting from (x, y) to the right, and the corresponding path in vector<int>
+static map< pair<int, int>, pair<int, vector<int>> >  memoization;
+
+
+pair<int, vector<int>> get_min(int r, int c) {
+    if (c < 0) {
+        return make_pair(0, vector<int> {});
     } else {
         int new_row_east = r;
         int new_row_northeast = (r - 1 < 0) ? row - 1 : r - 1;
         int new_row_southeast = (r + 1 >= row) ? 0 : r + 1;
 
-        int new_col = c + 1;
+        int new_col = c - 1;
 
-        int east = get_min(new_row_east, new_col);
-        int northeast = get_min(new_row_northeast, new_col);
-        int southeast = get_min(new_row_southeast, new_col);
+        vector<pair<int, vector<int>>> result;
+        
+        result.push_back( handle_memoization(new_row_northeast, new_col) );
+        result.push_back( handle_memoization(new_row_east, new_col) );
+        result.push_back( handle_memoization(new_row_southeast, new_col) );
+
+        auto min_it = min_element(result.begin(), result.end());
+
+        vector<int> second = min_it->second;
+        second.push_back(r);
+        return make_pair(data[r][c] + min_it->first, second);
+
 
         // char buffer[512];
         // sprintf(buffer, "c = %d,   r = %d, weight = %d\n", new_col, new_row_northeast, northeast);
@@ -49,7 +66,17 @@ int get_min(int r, int c) {
 
         // cout << buffer << endl;
 
-        return data[r][c] + min({ northeast, east, southeast });
+        // return data[r][c] + min_it->first;
+    }
+}
+
+pair<int, vector<int>> handle_memoization(int new_row, int new_col) {
+    if (memoization.find(make_pair(new_row, new_col)) == memoization.end()) {
+        auto p = get_min(new_row, new_col);
+        memoization[make_pair(new_row, new_col)] = p;
+        return p;
+    } else {
+        return memoization[make_pair(new_row, new_col)];
     }
 }
 
@@ -65,23 +92,43 @@ int main()
         }
 
         string line, token;
-        for (int i = 0; i < row; ++i) {
+        int i = 0, j = 0;
+        while (i < row) {
             getline(cin, line);
             stringstream ss(line);
 
-            for (int j = 0; j < col; ++j) {
-                getline(ss, token, ' ');
+            while (getline(ss, token, ' ')) {
                 data[i][j] = stoi(token);
+                ++j;
+                if (j >= col) {
+                    ++i;
+                    j = 0;
+                }
             }
         }
 
-        vector<int> result;
+        vector<pair<int, vector<int>>> result;
         for (int i = 0; i < row; ++i) {
-            result.push_back( get_min(i, 0) );
+            result.push_back( get_min(i, col - 1) );
         }
 
-        cout << *min_element(result.begin(), result.end()) << endl;
+        int value;
+        vector<int> path;
+        tie(value, path) = *min_element(result.begin(), result.end());
 
+        for (auto i = path.begin(); i != path.end(); ++i) {
+            cout << *i + 1;
+            if (i != path.end() - 1) {
+                cout << " ";
+            } else {
+                cout << endl;
+            }
+        }
+
+        cout << value << endl;
+
+        data.clear();
+        memoization.clear();
     }
 
     return 0;
