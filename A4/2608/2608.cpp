a1675a062bfda55b22d0be136b6e6832f6e57c52
Andy Zhang <zhihengz@asb9840u-a03.csil.sfu.ca>
2018-03-14 21:14:04 -0700

    2608

--- A4/2608/2608.cpp
+++ A4/2608/2608.cpp
@@ -3,70 +3,65 @@
 #include <vector>
 #include <cstring>
 using namespace std;
+// build array
+int n, maxSum, total;
+int c[14];
+bool isVisited[14];
 
-bool validPerm(int* clock, int n, int maxSum){
-  if (clock[n-1]+clock[n]+ clock[1] > maxSum){
-    return false;
+void Dfs(int current){
+  if (current == n){
+    if (c[current]+c[current-1]+ c[current-2] <= maxSum){
+      total = total + 1;// last triplet => this permutation is good
+    }
+    return;
   }
-  for (int i = 1; i <=n-2){
-      if (clock[i]+clock[i+1]+clock[i+2] > maxSum){
-        return false;
-      }
-  }
-  return true;
-}
-
-
-int dfs(int* clock, int* isVisited, int n, int maxSum, int total){
-  for (int i = 1 ; i <= n ; i++ ){
+  for (int i = 2 ; i <= n ; i++ ){ // check all triplets
     if (!isVisited[i]){
-      isVisited[i] = 1;
-      total = dfs(clock,isVisited,n,maxSum,total);
-      isVisited[i] = 0;
+        if (i +c[current-1]+ c[current-2] <= maxSum){
+          isVisited[i] = true;
+          c[current] = i;
+          Dfs(current+1);
+          isVisited[i] = false;
+      }
+      
     }
-  }
-  if (validPerm(clock, n, maxSum)){
-    return total+1; // increment counter if permutation <is valid
-  }
-  else{
-    return total;
   }
 }
 
-int perm(int n, int maxSum){
-  // build array
-  int clock[n+1];
-  int isVisited[n+1];
-  int total =0;
-  memset(isVisited,0,sizeof isVisited);
+void perm(){
+  memset(isVisited,false,sizeof isVisited);
   // DFS
-  // Fix 1 on top of clock, fix three elements on the top
-  clock[1] = 1;
-  isVisited[1] = 1;
-  // clock[1] = 1 < clock[n]< clock[2]
-  for (int i = 2; i <= n-1; i++){//clock[n]
-    clock[n] = i;
-    for (int j = i+1; j <=n;j++){//clock[2]
-      clock[2] = j;
-      if (i + j +1 < maxSum){
-        isVisited[i] = 1;
-        isVisited[j] = 1;
-        total = total + dfs(clock,isVisited,n,maxSum,0);
+  // Fix 1 on top of c, fix three elements on the top
+  total = 0;
+  c[1] = 1;
+  isVisited[1] = true;
+  // c[1] = 1 < c[n]< c[2]
+  for (int i = 2; i <= n-1; i++){//c[n]
+    for (int j = i+1; j <=n;j++){//c[2]
+      if (i + j +1 <= maxSum){
+        c[n] = i;
+        c[2] = j;
+        isVisited[i] = true;
+        isVisited[j] = true;
+        Dfs(3);
+        isVisited[i] = false;
+        isVisited[j] = false;
       }
     }
   }
-  return total;
 }
 
 int main(){
   int t;
   cin >> t; // Read the first number
   for (int i = 0 ; i < t; i++){
-    int n,maxSum;
     cin >> n >> maxSum; // This syntax would skip the space in the line
-    int p = perm(n, maxSum);
+    perm();
     cout <<"Permutation size:    " << n<<endl;
     cout <<"Maximum triplet sum: " << maxSum<<endl;
-    cout <<"Valid permutations:  " << p<<endl;
+    cout <<"Valid permutations:  " << total<<endl;
+    if (i < t -1)
+      cout << endl;
   }
+  return 0;
 }
